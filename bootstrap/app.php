4a23e8df0ed06eb81c872bdea7eb497a9a7d157a
GamsterPlace <gamsterplace@gmail.com>
2026-09-12 16:18:04 -0600

    eliminación de errores en cumplimiento de arqueos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'rol' => VerificarRol::class,
             'password.actualizada' => VerificarCambioPassword::class,
